241bce2f80f15738000da0e3921a5d4341b37152
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-12-02 21:54:22 -0800

    Refactor app class

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,22 +2,38 @@
 
 var auth = require('services/auth');
 var runManager = require('services/run-manager');
+var templates = require('templates');
 
-
-if (!auth.isLoggedIn()) {
-    window.location.href = 'login.html';
-} else {
-    runManager.getRun();
-
-    bindEvents();
-
+function App() {
 }
 
+App.prototype = {
+    template: templates['market-share'],
 
-function bindEvents() {
-    $('#logout').on('click', function () {
-        auth.logout().then(function () {
-            window.location = 'login.html';
+    initialize: function () {
+        if (!auth.isLoggedIn()) {
+            window.location.href = 'login.html';
+        } else {
+            runManager.getRun();
+
+            this.bindEvents();
+        }
+
+        return this;
+    },
+
+    bindEvents: function () {
+        $('#logout').on('click', function () {
+            auth.logout().then(function () {
+                window.location = 'login.html';
+            });
         });
-    });
-}
+    },
+
+    render: function () {
+        $('.market-share').html(this.template());
+    }
+};
+
+
+new App().initialize().render();
