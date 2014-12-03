5fa33042579136fcd3727fa9c08fe48d5f568136
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-12-02 16:46:15 -0800

    Redirect to login page if not logged in

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,5 +1,23 @@
 'use strict';
 
+var auth = require('services/auth');
 var runManager = require('services/run-manager');
 
-runManager.getRun();
+
+if (!auth.isLoggedIn()) {
+    window.location.href = 'login.html';
+} else {
+    runManager.getRun();
+
+    bindEvents();
+
+}
+
+
+function bindEvents() {
+    $('#logout').on('click', function () {
+        auth.logout(function () {
+            window.location = 'login.html';
+        });
+    });
+}
